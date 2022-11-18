eceed08137615c0030ee5d9ad2524f6e67d0f9e8
Paul Thouverey <paul.thouverey@etu.iut-tlse3.fr>
2022-11-18 12:21:25 +0100

    premier

---
+++ test_mini.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$qAjouterJoueur = 'INSERT INTO joueur (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
+                    VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
+
+
+?>
