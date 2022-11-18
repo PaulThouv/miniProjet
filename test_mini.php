482c0f573068a0653af10483ebe575668c62d75f
Paul Thouverey <paul.thouverey@etu.iut-tlse3.fr>
2022-11-18 14:05:35 +0100

    Deuxieme

--- test_mini.php
+++ test_mini.php
@@ -4,5 +4,6 @@
 $qAjouterJoueur = 'INSERT INTO joueur (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
                     VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
 
-
+$qAjouterMatch = 'INSERT INTO unmatch (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
+VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
 ?>
