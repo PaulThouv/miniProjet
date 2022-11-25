aaab80e8ee1ec7f8802f40183a2673cbba3c1ffa
Paul Thouverey <paul.thouverey@etu.iut-tlse3.fr>
2022-11-25 11:14:55 +0100

    TRUC

--- test_mini.php
+++ test_mini.php
@@ -6,4 +6,6 @@ $qAjouterJoueur = 'INSERT INTO joueur (Nom,Prenom,Numero_Licence,Photo,Date_Nais
 
 $qAjouterMatch = 'INSERT INTO unmatch (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
 VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
+
+echo("mabite");
 ?>
