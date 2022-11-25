5e90020e57ab25f03bd21aa25589fda6724699c8
Matthis Van de vyver <matthis.van-de-vyver@etu.iut-tlse3.fr>
2022-11-25 11:22:45 +0100

    Matthis image

--- test_mini.php
+++ test_mini.php
@@ -7,5 +7,35 @@ $qAjouterJoueur = 'INSERT INTO joueur (Nom,Prenom,Numero_Licence,Photo,Date_Nais
 $qAjouterMatch = 'INSERT INTO unmatch (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
 VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
 
+function uploadImage($photo)
+{
+
+    if (isset($photo)) {
+        $tmpName = $photo['tmp_name'];
+        $name = $photo['name'];
+        $size = $photo['size'];
+        $error = $photo['error'];
+
+        $tabExtension = explode('.', $name);
+        $extension = strtolower(end($tabExtension));
+
+        $extensions = ['jpg', 'png', 'jpeg', 'gif', 'svg', 'webp', 'bmp'];
+        $maxSize = 400000;
+
+        if (in_array($extension, $extensions) && $size <= $maxSize && $error == 0) {
+
+            $uniqueName = uniqid('', true);
+            //uniqid génère quelque chose comme ca : 5f586bf96dcd38.73540086
+            $file = $uniqueName . "." . $extension;
+            $chemin = "upload/";
+            //$file = 5f586bf96dcd38.73540086.jpg
+            move_uploaded_file($tmpName, 'upload/' . $file);
+            $result = $chemin . $file;
+        }
+    } else {
+        echo '<h1>erreur</h1>';
+    }
+    return $result;
+}
+
 echo("mabite");
-?>
