77ef375dbd9bc4270fcf1845617a5cd26cd7abbf
Matthis Van de vyver <matthis.van-de-vyver@etu.iut-tlse3.fr>
2022-11-25 11:27:15 +0100

    dede

--- test_mini.php
+++ test_mini.php
@@ -7,6 +7,17 @@ $qAjouterJoueur = 'INSERT INTO joueur (Nom,Prenom,Numero_Licence,Photo,Date_Nais
 $qAjouterMatch = 'INSERT INTO unmatch (Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere,Statut,Commentaires) 
 VALUES (:nom , :prenom,:numerolicence ,:dateNaissance,:taille,:poids,:posteprefere,:statut, :commentaires)';
 
+// requete pour ajouter un Joueur a la BD
+$qAjouterJoueur = 'INSERT INTO joueur(Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere_Statut_Commentaires) 
+                    VALUES (:nom , :prenom, :dateNaissance, :lienJeton)';
+
+// requete pour verifier qu'un enfant avec les données en parametre n'existe pas deja dans la BD
+$qEnfantIdentique = 'SELECT Nom, Prenom, Date_Naissance FROM enfant 
+                    WHERE Nom = :nom AND Prenom = :prenom AND Date_Naissance = :dateNaissance';
+
+// requete pour afficher le nom prenom de tous les enfants dont un membre s'occupe (pour le moment ca affiche tout le monde)
+$qAfficherNomPrenomEnfant = 'SELECT Id_Enfant, Nom,Prenom FROM Enfant ORDER BY Nom';
+
 function uploadImage($photo)
 {
 
@@ -37,14 +48,3 @@ function uploadImage($photo)
     }
     return $result;
 }
-
-// requete pour ajouter un Joueur a la BD
-$qAjouterJoueur = 'INSERT INTO joueur(Nom,Prenom,Numero_Licence,Photo,Date_Naissance,Taille,Poids,Poste_Prefere_Statut_Commentaires) 
-                    VALUES (:nom , :prenom, :dateNaissance, :lienJeton)';
-
-// requete pour verifier qu'un enfant avec les données en parametre n'existe pas deja dans la BD
-$qEnfantIdentique = 'SELECT Nom, Prenom, Date_Naissance FROM enfant 
-                    WHERE Nom = :nom AND Prenom = :prenom AND Date_Naissance = :dateNaissance';
-
-// requete pour afficher le nom prenom de tous les enfants dont un membre s'occupe (pour le moment ca affiche tout le monde)
-$qAfficherNomPrenomEnfant = 'SELECT Id_Enfant, Nom,Prenom FROM Enfant ORDER BY Nom';
