4bd36f80a629b9b7029e2e59cc21072bbf097668
Matthis Van de vyver <matthis.van-de-vyver@etu.iut-tlse3.fr>
2022-11-25 11:33:05 +0100

    dededede

--- QUERY.php
+++ QUERY.php
@@ -13,10 +13,51 @@ $qAjouterJoueur = 'INSERT INTO joueur(Nom,Prenom,Numero_Licence,Photo,Date_Naiss
 
 // requete pour verifier qu'un enfant avec les données en parametre n'existe pas deja dans la BD
 $qJoueurIdentique = 'SELECT Nom, Prenom, Date_Naissance FROM joueur 
-                    WHERE Nom = :nom AND Prenom = :prenom AND Date_Naissance = :dateNaissance AND Numero_Licence = :numeroLicence' ;
+                    WHERE Nom = :nom AND Prenom = :prenom AND Date_Naissance = :dateNaissance AND Numero_Licence = :numeroLicence';
 
 // requete pour afficher le nom prenom de tous les enfants dont un membre s'occupe (pour le moment ca affiche tout le monde)
 $qAfficherNomPrenomJoueur = 'SELECT Id_Joueur, Nom,Prenom FROM Enfant ORDER BY Nom';
+
+// fonction qui permet de se connecter a la BD
+function connexionBd()
+{
+    // parametre de connexion a la BD
+    // cDRvPP\2mwea(LGp
+    // https://test-saetrisomie21.000webhostapp.com/
+    $SERVER = '127.0.0.1';
+    $DB = 'equipe_rugby';
+    $LOGIN = 'root';
+    $MDP = '';
+    // tentative de connexion a la BD
+    try {
+        // connexion a la BD
+        $linkpdo = new PDO("mysql:host=$SERVER;dbname=$DB", $LOGIN, $MDP);
+    } catch (Exception $e) {
+        die('Erreur ! Probleme de connexion a la base de donnees' . $e->getMessage());
+    }
+    return $linkpdo;
+}
+
+// fonction qui vérifie que les champs $_POST sont bien remplis
+function champRempli($field)
+{
+    // parcoure la liste des champs 
+    foreach ($field as $name) {
+        // vérifie s'ils sont vides 
+        if (empty($_POST[$name])) {
+            return false; // au moins un champs vides
+        }
+    }
+    return true; // champs remplis
+}
+
+// fonction qui permet d'enlever les balises de code dans les champs
+function clean($champEntrant)
+{
+    $champEntrant = strip_tags($champEntrant); // permet d'enlever les balises html, xml, php
+    $champEntrant = htmlspecialchars($champEntrant); // permet de transformer les balises html en *String
+    return $champEntrant;
+}
 
 function uploadImage($photo)
 {
@@ -47,4 +88,33 @@ function uploadImage($photo)
         echo '<h1>Erreur ! Problème lors de l\'upload de l\'image</h1>';
     }
     return $result;
+}
+
+// -----------------------------------------------Enfant--------------------------------------------------------------------
+
+// fonction qui permet d'ajouter un enfant a la BD
+function ajouterEnfant(
+    $nom,
+    $prenom,
+    $dateNaissance,
+    $lienJeton
+) {
+
+    // connexion a la BD
+    $linkpdo = connexionBd();
+    // preparation de la requete sql
+    $req = $linkpdo->prepare($GLOBALS['qAjouterEnfant']);
+    if ($req == false) {
+        die('Erreur ! Il y a un probleme lors de la preparation de la requete pour ajouter un enfant a la BD');
+    }
+    // execution de la requete sql
+    $req->execute(array(
+        ':nom' => clean($nom),
+        ':prenom' => clean($prenom),
+        ':dateNaissance' => clean($dateNaissance),
+        ':lienJeton' => clean($lienJeton)
+    ));
+    if ($req == false) {
+        die('Erreur ! Il y a un probleme lors l\'execution de la requete pour ajouter un enfant a la BD');
+    }
 }
